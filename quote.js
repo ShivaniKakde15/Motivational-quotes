2f330b8357f43a261c15b77abdef6fc8304caedc
ShivaniKakde15 <110591443+ShivaniKakde15@users.noreply.github.com>
2022-08-05 17:56:44 +0530

    quote project

---
+++ quote.js
@@ -0,0 +1,31 @@
+// var quotes=[
+//     "Life is 10% what happens to you and 90% how you react to it.<br> —Charles R. Swindoll",
+//     "Good, better, best. Never let it rest. 'Til your good is better and your better is best.<br> —St.Jerome",
+//     "You can get everything in life you want if you will just help enough other people get what they want.<br> —Zig Ziglar",
+//     "Inspiration does exist, but it must find you working.<br> —Pablo Picasso",
+//     "I have stood on a mountain of no’s for one yes.<br> —Barbara Elaine Smith",
+//     "If you believe something needs to exist, if it's something you want to use yourself, don't let anyone ever stop you from doing it.<br> —Tobias Lütke",
+//     "The battles that count aren't the ones for gold medals. The struggles within yourself—the invisible, inevitable battles inside all of us—that's where it's at.<br> —Jesse Owens",
+//     "I have never let my schooling interfere with my education.<br> —Mark Twain",
+//     "Life is like riding a bicycle. To keep your balance you must keep moving.<br> —Albert Einstein",
+//     "Develop success from failures. Discouragement and failure are two of the surest stepping stones to success.<br> —Dale Carnegie"
+// ]
+// function getQuote(){
+//     var randomQuote=Math.floor(Math.random() * quotes.length);
+//     document.getElementById('new').innerHTML = quotes[randomQuote];
+// }
+
+
+
+// const image=document.querySelector("#image");
+// let isVisible = true;
+// function toggleImage() {
+//     isVisible = !isVisible;
+//     console.log(isVisible);
+//     if (isVisible){
+//             image.src="image8.png";
+//     }else {
+//             image.src="image9.png";
+//     }
+// }
+// toggleImage();
